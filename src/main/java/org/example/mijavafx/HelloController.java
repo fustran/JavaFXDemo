17a096f27c661ca1ee5f86b20f9502c1a90f11ce
fustran <ifussim@outlook.com>
2025-04-11 18:57:33 +0200

    Empezando con JavaFX

---
+++ src/main/java/org/example/mijavafx/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.mijavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
